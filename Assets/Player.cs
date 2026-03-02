b356921946fb9e52ce755b95e9f9895d42f8258b
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-03-02 11:39:23 +0100

    gitattributes for assets added

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,12 +6,17 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Start was called");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update was called");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate was called");
     }
 }
